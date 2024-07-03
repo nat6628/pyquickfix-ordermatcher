5c59fa03d6163ebccbf8cf2b2e395a8ec2345b28
Hung Do <hungdo@cqtdt.asia>
2024-07-03 20:53:12 +0700

    database

--- sql/query.sql
+++ sql/query.sql
@@ -1,6 +1,6 @@
-DELETE FROM pending_order WHERE 1=1;
-DELETE FROM order_history WHERE 1=1;
+-- DELETE FROM pending_order WHERE 1=1;
+-- DELETE FROM order_history WHERE 1=1;
 --SELECT clOrdID, status FROM order_history WHERE clOrdID > 0;
 --drop table order_history;
-
+DELETE FROM pending_order WHERE insertTime ='2024-07-03 20:24:13.454178';
 -- SELECT clOrdID, status FROM order_history WHERE notify = 'FALSE'
